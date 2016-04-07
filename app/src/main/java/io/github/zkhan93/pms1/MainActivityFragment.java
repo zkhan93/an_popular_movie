9631ccf732896ac10424c484a78aa16b63271e9c
Zeeshan Khan <zkhan1093@gmail.com>
2016-04-07 13:37:00 +0530

    added recycler view and card view

--- app/src/main/java/io/github/zkhan93/pms1/MainActivityFragment.java
+++ app/src/main/java/io/github/zkhan93/pms1/MainActivityFragment.java
@@ -1,7 +1,8 @@
 package io.github.zkhan93.pms1;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +11,7 @@ import android.view.ViewGroup;
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+    private RecyclerView movieList;
 
     public MainActivityFragment() {
     }
@@ -17,6 +19,13 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        movieList = (RecyclerView) rootView.findViewById(R.id.movie_list);
+        return rootView;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
     }
 }
