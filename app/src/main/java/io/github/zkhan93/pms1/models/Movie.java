c6c21e749147282760c79e33d2b79a214715a1da
Zeeshan Khan <zkhan1093@gmail.com>
2016-04-07 13:22:41 +0530

    initial commit

---
+++ app/src/main/java/io/github/zkhan93/pms1/models/Movie.java
@@ -0,0 +1,53 @@
+package io.github.zkhan93.pms1.models;
+
+import java.util.Date;
+
+/**
+ * Model to store a movie detail.
+ * Created by Zeeshan Khan on 4/7/2016.
+ */
+public class Movie {
+    private String title, overview, poster_path;
+    private Date releaseDate;
+    private float rating;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+    public String getPoster_path() {
+        return poster_path;
+    }
+
+    public void setPoster_path(String poster_path) {
+        this.poster_path = poster_path;
+    }
+
+    public Date getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(Date releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public float getRating() {
+        return rating;
+    }
+
+    public void setRating(float rating) {
+        this.rating = rating;
+    }
+}
