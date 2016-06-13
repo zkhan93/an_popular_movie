86e131c050a684285dc7df4045dd5a61f42fb8ae
Zeeshan Khan <zkhan1093@gmail.com>
2016-06-13 22:46:49 +0530

    almost working tablet ui working, db storage

---
+++ app/src/main/java/io/github/zkhan93/pm/data/Contract.java
@@ -0,0 +1,7 @@
+package io.github.zkhan93.pm.data;
+
+/**
+ * Created by Zeeshan Khan on 6/13/2016.
+ */
+public class Contract {
+}
---
+++ app/src/main/java/io/github/zkhan93/pm/models/FavClickCallback.java
@@ -0,0 +1,7 @@
+package io.github.zkhan93.pm.models;
+
+/**
+ * Created by Zeeshan Khan on 6/13/2016.
+ */
+public class FavClickCallback {
+}
